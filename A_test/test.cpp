f8406b76e1c7e905ab3beab46fe292a6bc88aadc
unknown <2395967268@qq.com>
2024-08-29 12:46:30 +0800

    update test file

--- A_test/test.cpp
+++ A_test/test.cpp
@@ -1,22 +1,11 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-class Solution {
-public:
-    int numSquares(int n) {
-        vector<int> dp(n+1, INT_MAX);
-        dp[0] = 0;
-        for (int i = 1; i <= n; i++) 
-            for (int j = 1; j*j <= i; j++) 
-                if (dp[i - j*j] != INT_MAX)
-                    dp[i] = min(dp[i - j*j] + 1, dp[i]);
-        return dp[n];
-    }
-};
+void func() {
+    cout << "test ok" << endl;
+}
 
 int main() {
-    Solution s;
-    cout << s.numSquares(12) << endl;
+    func();
     return 0;
 }
